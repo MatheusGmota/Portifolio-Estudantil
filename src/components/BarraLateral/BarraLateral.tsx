f7b20d1c344a01f819b67452bdfcfade0ec89878
felipeSeiki <felipe.hashiguti@gmail.com>
2024-10-21 21:02:18 -0300

    Estilizando a sidebar

--- src/components/BarraLateral/BarraLateral.tsx
+++ src/components/BarraLateral/BarraLateral.tsx
@@ -3,32 +3,34 @@
 import React, { useState } from 'react'
 import { TipoAluno } from "@/types";
 import Image from 'next/image';
+import Link from 'next/link';
 
 export default function BarraLateral() {
 
     const [aluno, setAluno] = useState<TipoAluno>({
-        nome: 'Felipe Seiki', curso: 'Ads', img: '/img/foto-felipe.jpg', rm: '98985', turma: '1TDSPY'
+        nome: 'Felipe Seiki', curso: 'Análise e Desenvolvimento de Sistemas', img: '/img/foto-felipe.jpg', rm: '98985', turma: '1TDSPY'
     });
 
     return (
         <div className='barra-lateral'>
-            {/* {aluno.map((a) =>( */}
-            <div key={aluno.rm} className='wrapper-aluno'>
+            <section key={aluno.rm} className='wrapper-aluno'>
                 <article>
                     <div className='aluno'>
-                        <Image src={aluno.img} width={140} height={140} alt={`foto de ${aluno.nome}`} />
+                        <Image src={aluno.img} width={140} height={140} alt={`foto de ${aluno.nome}`} className='rounded-full m-2'/>
                         <h3>
                             {aluno.nome}
                         </h3>
                     </div>
                     <div className='dados'>
-                        <p>{aluno.rm}</p>
-                        <p>{aluno.turma}</p>
-                        <p>curso</p>
-                        <p>{aluno.curso}</p>
+                        <p>RM - {aluno.rm}</p>
+                        <p>Turma - {aluno.turma}</p>
+                        <p>Curso - {aluno.curso}</p>
                     </div>
                 </article>
-            </div>
+                <div>
+                    <Link href={"/"}>Voltar</Link>
+                </div>
+            </section>
 
         </div>
     )
