831e8be1edd7daae56d8c2598b5e21d6b0663e0f
lucas-corradini <lucas.corradini.2003@gmail.com>
2024-10-24 20:07:58 -0300

    finalizando primeria parte da pagina

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,51 +1,46 @@
+"use-client"
+
+
 export default function Home() {
   return (
-    <main>
+    <>
+      <main className="pagina-inicial">
+          <h1 className="titulo">
+              Portfólio Estudantil
+          </h1>
+          <section className="area">
+              
+              <article className="box">
+                  <h2 className="sub-titulos">
+                      Checkpoint ( CPS )
+                  </h2>
+                  <p className="texto">
+                      Avaliações intermediárias para acompanhamento do progresso.
+                  </p>
+              </article>
 
-        <h1>
-          Portifólio Estudantil
-        </h1>
+              <article className="box">
+                  <h2 className="sub-titulos">
+                      Global Solutions ( GS )
+                  </h2>
+                  <p className="texto">
+                      Projetos ou avaliações integradoras, que envolvem a aplicação global dos conhecimentos adquiridos.
+                  </p>
+              </article>
 
-        <section>
-
-          <article>
-            <aside>
-              <h2>
-                Checkpoint ( CPS )
-              </h2>
-            </aside>
-            <p>
-              Avaliações intermediárias para acompanhamento do progresso.
-            </p>
-          </article>
-
-          <article>
-            <aside>
-              <h2>
-                Global Solutions ( GS )
-              </h2>
-            </aside>
-            <p>
-              Projetos ou avaliações integradoras, que envolvem a aplicação global dos conhecimentos adquiridos.
-            </p>
-          </article>
-
-          <article>
-            <aside>
-              <h2>
-                Challenge Sprints
-              </h2>
-            </aside>
-            <p>
-              Avaliações em formato de desafios, com foco em resolução de problemas com entregas programadas.
-            </p>
-          </article>
-
-        </section>
-
-        <section>
-          CARDS
-        </section>
-    </main>
+              <article className="box">
+                  <h2 className="sub-titulos">
+                      Challenge Sprints
+                  </h2>
+                  <p className="texto">
+                      Avaliações em formato de desafios, com foco em resolução de problemas com entregas programadas.
+                  </p>
+              </article>
+          </section>
+          <section>
+            
+          </section>
+      </main>
+    </>
   );
 }
