6c5f21fa5a43da61e57eb7bd4ce75c3593a9437c
lucas-corradini <lucas.corradini.2003@gmail.com>
2024-10-24 20:08:32 -0300

    implementando metodo GET

---
+++ src/app/api/base-alunos/route.ts
@@ -0,0 +1,11 @@
+import { TipoAluno } from "@/types";
+import { promises as fs } from "fs";
+import { NextResponse } from "next/server";
+
+export async function GET(request: Request){
+    const file = await fs.readFile(process.cwd() + '/src/data/alunos.json', 'utf-8');
+    
+    const dados:TipoAluno[] = JSON.parse(file);
+
+    return NextResponse.json(dados, {status: 200});
+}
---
+++ src/types.ts
@@ -0,0 +1,7 @@
+export type TipoAluno = {
+    rm: string,
+    img: string,
+    nome: string,
+    turma: string,
+    curso: string
+}
