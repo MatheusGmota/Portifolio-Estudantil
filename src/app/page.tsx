3f71b45888ba53d2c91f095b30369bf1b3776e4a
lucas-corradini <lucas.corradini.2003@gmail.com>
2024-10-24 18:54:30 -0300

    primerio commit com mudanças iniciais

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,51 @@
 export default function Home() {
   return (
-    <></>
+    <main>
+
+        <h1>
+          Portifólio Estudantil
+        </h1>
+
+        <section>
+
+          <article>
+            <aside>
+              <h2>
+                Checkpoint ( CPS )
+              </h2>
+            </aside>
+            <p>
+              Avaliações intermediárias para acompanhamento do progresso.
+            </p>
+          </article>
+
+          <article>
+            <aside>
+              <h2>
+                Global Solutions ( GS )
+              </h2>
+            </aside>
+            <p>
+              Projetos ou avaliações integradoras, que envolvem a aplicação global dos conhecimentos adquiridos.
+            </p>
+          </article>
+
+          <article>
+            <aside>
+              <h2>
+                Challenge Sprints
+              </h2>
+            </aside>
+            <p>
+              Avaliações em formato de desafios, com foco em resolução de problemas com entregas programadas.
+            </p>
+          </article>
+
+        </section>
+
+        <section>
+          CARDS
+        </section>
+    </main>
   );
 }
