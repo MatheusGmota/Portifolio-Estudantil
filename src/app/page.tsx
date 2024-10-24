d52c3fdbcf61eb3d4034a12168ce668e8c614f97
lucas-corradini <lucas.corradini.2003@gmail.com>
2024-10-24 20:52:28 -0300

    Página até em tão finalizada

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,29 @@
-"use-client"
+"use client"
+
+import Card from "@/components/Card/Card";
+import { TipoAluno } from "@/types";
+import { useEffect, useState } from "react";
+import { motion } from "framer-motion";
 
 
 export default function Home() {
+
+  const [alunos, setAlunos] = useState<TipoAluno[]>([]);
+  
+  useEffect(() => {
+    const chamadaApi = async ()=>{
+      const response = await fetch(`http://localhost:3000/api/base-alunos`)
+      if(response.ok){
+        const data = await response.json();
+        setAlunos(data);
+      }
+    }
+    chamadaApi();
+  },[])
   return (
     <>
-      <main className="pagina-inicial">
+      <main>
+        <motion.div  initial={{ x: -100, opacity: 0 }} animate={{ x: 0, opacity: 1 }} transition={{ ease: "easeInOut", duration: 0.7 }} className="pagina-inicial">
           <h1 className="titulo">
               Portfólio Estudantil
           </h1>
@@ -37,9 +56,14 @@ export default function Home() {
                   </p>
               </article>
           </section>
-          <section>
-            
+          <section className="card-area">
+            {alunos.map((a,i) =>(
+              <div key={i} className="m-20">
+                <Card aluno={a}/>
+              </div>
+            ) )}
           </section>
+        </motion.div>
       </main>
     </>
   );
